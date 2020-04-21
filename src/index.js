fde23942f0dea6a15c7701671ef92f5d13961a34
nasko25 <apashov0@gmail.com>
2020-04-21 14:27:51 +0200

    started designing the messaging view

--- src/index.js
+++ src/index.js
@@ -4,10 +4,62 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class MainView extends React.Component {
+  render() {
+    return (
+      <div className = "mainView">
+        <MessageView/>
+        <ComposeView/>
+      </div>
+    );
+  };
+}
+
+class MessageView extends React.Component {
+  render() {
+    return (
+      <div> This is the messages view </div>
+    );
+  };
+}
+
+class ComposeView extends React.Component {
+  render() {
+    return (
+      <div className = "compose">
+        <input
+         type="text"
+         className="input"
+         placeholder="Message"
+       />
+        <SendButton />
+      </div>
+    );
+  };
+}
+
+class SendButton extends React.Component {
+  render() {
+    return (
+      <button className = "send">
+        <i className="material-icons center">send</i>
+      </button>
+    );
+  };
+}
+
+
+// ============================================
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MainView />,
   document.getElementById('root')
 );
 
