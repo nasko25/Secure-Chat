cf651b768798ca0b3252d6f1cfd477f37c811047
nasko25 <apashov0@gmail.com>
2020-05-10 14:18:27 +0300

    added a loader and delay after clicking on the link

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  withRouter
 } from "react-router-dom";
 import MainView from "./MainView.js"
 import './index.css'
@@ -11,11 +12,12 @@ import './index.css'
 
 // TODO https://reacttraining.com/react-router/web/example/query-parameters     query parameters (chat id will be a parameter)
 export default function App() {
+  const InitilizeConnectionWithRouter =  withRouter(InitilizeConnection);
   return (
     <Router>
       <Switch>
         <Route exact path = "/">
-          <InitilizeConnection/>
+          <InitilizeConnectionWithRouter/>
         </Route>
         <Route path = "/chat">
           <MainView />
@@ -25,17 +27,30 @@ export default function App() {
   );
 }
 
-function InitilizeConnection() {
-  return (
-    <div className = "indexPage mainView">
-      <div className="box">
-        <input type="input" className="secretField" placeholder="Secret" name="secret" id='secret'/>
-        <label htmlFor="secret" className="secretLabel">Secret</label>
+class InitilizeConnection extends React.Component {
+  initiateConnection = (to, event)=> {
 
-        <div className="readyBtn">
-          <Link className="readyLink" to = "chat"> Ready </Link>
+    // TODO id instead of class?
+    document.getElementsByClassName("readyLink")[0].style.display = "none";
+    document.getElementById("load").style.display = "inline-block";
+
+    event.preventDefault();
+    setTimeout(()=>this.props.history.push(to), 5000);
+  }
+
+  render() {
+    return (
+      <div className = "indexPage mainView">
+        <div className="box">
+          <input type="input" className="secretField" placeholder="Secret" name="secret" id='secret'/>
+          <label htmlFor="secret" className="secretLabel">Secret</label>
+
+          <div className="readyBtn">
+            <Link className="readyLink" to = "chat" onClick={(event) => this.initiateConnection({ pathname: `chat`, /* hash: `#hash`, */ }, event)}> Ready </Link>
+            <div className="loader" id = "load"><div></div><div></div><div></div><div></div></div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
