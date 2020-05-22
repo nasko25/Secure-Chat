27237bfb396e54eb6eab58371375503696cd9be2
nasko25 <apashov0@gmail.com>
2020-05-22 22:11:07 +0300

    added client classes on the server
    also made the token list an object where each entry will be a token-ClientPair object
    
    ClientPairs will contain two Client classes and additional information like number of connections and when the pair was last in use (so that later I will implement garbage collection)
    
    the client classes will contain the socket and public key of the client

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ const app = express();
 const port = process.env.PORT || 9000;
 const bodyParser = require("body-parser");
 
+function Client(socket, publicKey) {
+	this.socket = socket;
+	this.publicKey = publicKey;
+}
+
+function ClientPair(client1, client2) {
+	this.client1 = client1;
+	this.client2 = client2;
+	this.connections = 0;
+	this.lastUsed = Date.now();
+}
+
 // TODO clear the tokens that have stayed for too long
-let tokens = [];
+let tokens = {};
 
 app.use(bodyParser.json());
 
@@ -18,7 +30,7 @@ app.get("/api", (req, res) => {
 app.post("/verify_token", (req, res) => {
 	console.log(req.body);
 	let token = req.body.token;
-	if (!(tokens.includes(token))) {
+	if (!(token in tokens)) {
 		res.status(400).send({message: "Invalid token"});
 		//res.send("Sorry the token is invalid.\nThis might be caused by an expired session or just by an invalid token provided.")
 	} else{
@@ -35,9 +47,25 @@ app.get("/generate_token", (req, res) => {
 	  if (buffer) {
 		let token = buffer.toString('hex');
 		res.send({token: token});
-		tokens.push(token);
+		// TODO collisions?
+		tokens[token] = new ClientPair();
 	  }
 	  else
 		res.end();
 	});
 });
+
+/*
+		if (!(token in tokens)) {
+			var client = new Client("TODO:socket", "TODO:public key");
+			tokens[token] = new ClientPair(client, null);
+		}
+		else {
+			var clientPair = tokens[token];
+			if (clientPair.client2 == null && clientPair.connections < 2) {
+				clientPair.client2 = new Client("TODO:socket", "TODO:public key");
+				clientPair.connections++;
+			}
+		}
+
+*/
